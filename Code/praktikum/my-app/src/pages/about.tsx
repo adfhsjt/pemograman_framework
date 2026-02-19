3a23e01342ba43751c7a6b7fe9ce5dfdffd07f8e
adfhsjt <ahmadriski207@gmail.com>
2026-02-19 14:33:25 +0700

    jobsheet 1: setup

---
+++ Code/praktikum/my-app/src/pages/about.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>About Page</h1> <br />
+      <p>Ini adalah halaman About</p>
+      <p>Nama: Ahmad Dzul Fadhli Hannan</p>
+      <p>NIM: 2341720106</p>
+      <p>Program Studi: D4 Teknik Informatika</p>
+    </div>
+  )
+}

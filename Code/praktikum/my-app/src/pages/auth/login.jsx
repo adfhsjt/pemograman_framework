af203b47b64ee4f2048d41d82c3b576a851c0fde
adfhsjt <ahmadriski207@gmail.com>
2026-02-25 15:45:57 +0700

    Langkah 7 – Navigasi Imperatif (router.push)

--- Code/praktikum/my-app/src/pages/auth/login.jsx
+++ Code/praktikum/my-app/src/pages/auth/login.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const halamanLogin = () => {
+    const {push} = useRouter();
+    const handlerLogin = () => {
+        push("/produk");
+    }
     return (
         <div>
             <h1>Halaman Login</h1>
+            <button onClick={handlerLogin}>Login</button> <br />
+            <button onClick={() => push('/produk')}>Login</button> <br />
+            <button onClick={() => handlerLogin()}>Login</button> <br />
             <Link href="/auth/register">Ke Halaman Register</Link>
         </div>
     )

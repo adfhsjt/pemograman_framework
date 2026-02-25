cd760f9de0b0cfc3b834b84eb990af5ea0f643d5
adfhsjt <ahmadriski207@gmail.com>
2026-02-25 22:00:43 +0700

    3. Styling untuk Pages (CSS Module)

--- Code/praktikum/my-app/src/pages/auth/login.jsx
+++ Code/praktikum/my-app/src/pages/auth/login.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./login.module.css";
 
 const halamanLogin = () => {
     const {push} = useRouter();
@@ -7,10 +8,10 @@ const halamanLogin = () => {
         push("/produk");         {/* Login → Product (imperatif) */}
     }
     return (
-        <div>
+        <div className={styles.login}>
             <h1>Halaman Login</h1>
-            <button onClick={handlerLogin}>Login</button> <br /> {/* Login → Product (imperatif) */}
-            <button onClick={() => push('/produk')}>Login</button> <br />
+            {/* <button onClick={handlerLogin}>Login</button> <br /> Login → Product (imperatif) */}
+            {/* <button onClick={() => push('/produk')}>Login</button> <br /> */}
             <button onClick={() => handlerLogin()}>Login</button> <br />
 
             <Link href="/auth/register">Ke Halaman Register</Link> {/* Login → Register (link) */}

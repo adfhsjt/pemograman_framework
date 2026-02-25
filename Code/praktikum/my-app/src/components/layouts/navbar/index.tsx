e651e8fb4d617ccf867e666ba4766ad1d5679cd6
adfhsjt <ahmadriski207@gmail.com>
2026-02-25 21:54:42 +0700

    Jobsheet 5: 1. Global CSS, 2. CSS Module (Local Scope)

--- Code/praktikum/my-app/src/components/layouts/navbar/index.tsx
+++ Code/praktikum/my-app/src/components/layouts/navbar/index.tsx
@@ -1,6 +1,8 @@
+import syles from "./navbar.module.css";
+
 const Navbar = () => {
     return (
-        <div className="navbar">
+        <div className={syles.navbar}>
             <div>Navbar Component </div>
         </div>
     );

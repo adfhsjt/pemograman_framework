8939b22412d3571813ca2d1580655921b6756522
adfhsjt <ahmadriski207@gmail.com>
2026-02-26 02:40:46 +0700

    6. Inline Styling (CSS-in-JS)

--- Code/praktikum/my-app/src/pages/views/auth/login/index.tsx
+++ Code/praktikum/my-app/src/pages/views/auth/login/index.tsx
@@ -12,6 +12,8 @@ const TampilanLogin = () => {
         <div className={styles.login}>
             <h1>Halaman Login</h1>
             <button onClick={handlerLogin}>Login</button> <br />
+            {/* Inline Styling (CSS-in-JS) */}
+            <h1 style={{ color: "red",border: "1px solid red", borderRadius: "10px",padding: "10px",}}>Belum Punya Akun</h1>
             <Link href={"/auth/register"}>Ke Halaman Register</Link>
         </div>
     );

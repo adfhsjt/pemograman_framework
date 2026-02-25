4289d44182223ccc2d26b28743e076c011f66f5b
adfhsjt <ahmadriski207@gmail.com>
2026-02-25 16:48:41 +0700

    Tugas 2

--- Code/praktikum/my-app/src/pages/auth/login.jsx
+++ Code/praktikum/my-app/src/pages/auth/login.jsx
@@ -4,15 +4,16 @@ import { useRouter } from "next/router";
 const halamanLogin = () => {
     const {push} = useRouter();
     const handlerLogin = () => {
-        push("/produk");
+        push("/produk");         {/* Login → Product (imperatif) */}
     }
     return (
         <div>
             <h1>Halaman Login</h1>
-            <button onClick={handlerLogin}>Login</button> <br />
+            <button onClick={handlerLogin}>Login</button> <br /> {/* Login → Product (imperatif) */}
             <button onClick={() => push('/produk')}>Login</button> <br />
             <button onClick={() => handlerLogin()}>Login</button> <br />
-            <Link href="/auth/register">Ke Halaman Register</Link>
+
+            <Link href="/auth/register">Ke Halaman Register</Link> {/* Login → Register (link) */}
         </div>
     )
 }

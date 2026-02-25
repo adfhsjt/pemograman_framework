7aa98d893101c80a865cfff683e1552c9b047003
adfhsjt <ahmadriski207@gmail.com>
2026-02-25 22:13:50 +0700

    4. Conditional Rendering Navbar (Tanpa Navbar di Login)

--- Code/praktikum/my-app/src/components/layouts/Appshell/index.tsx
+++ Code/praktikum/my-app/src/components/layouts/Appshell/index.tsx
@@ -1,4 +1,7 @@
+import { useRouter } from "next/router";
 import Navbar from "../navbar";
+
+const disableNavbar = ['/auth/login', '/auth/register'];
 
 type AppShellProps = {
     children: React.ReactNode;
@@ -6,13 +9,14 @@ type AppShellProps = {
 
 const AppShell = (props:AppShellProps) => {
     const {children} = props;
+    const {pathname} = useRouter();
+    // Insight: Gunakan router.pathname jika ada query parameter untuk memastikan path yang tepat
+    // const router = useRouter();
+    // console.log(router);
     return (
         <main>
-            <Navbar />
+            {!disableNavbar.includes(pathname) && <Navbar />}
             {children}
-            <div>
-                footer
-            </div>
         </main>
     );
 };

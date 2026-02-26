0c0688583acfbee778dd1f6de40cefe392d72f9c
adfhsjt <ahmadriski207@gmail.com>
2026-02-26 08:28:53 +0700

    9. Tailwind CSS

--- Code/praktikum/my-app/src/pages/views/auth/login/index.tsx
+++ Code/praktikum/my-app/src/pages/views/auth/login/index.tsx
@@ -11,7 +11,7 @@ const TampilanLogin = () => {
     }
     return (
         <div className={styles.login}>
-            <h1>Halaman Login</h1>
+            <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
             <button onClick={handlerLogin}>Login</button> <br />
             {/* Inline Styling (CSS-in-JS) */}
             <h1 style={{ color: "red",border: "1px solid red", borderRadius: "10px",padding: "10px",}}>Belum Punya Akun</h1>

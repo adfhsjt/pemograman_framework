f5f461397a5d30b5c127e54f4be6f5ab8a15aacd
adfhsjt <ahmadriski207@gmail.com>
2026-03-12 13:39:53 +0700

    Bagian 4 – Batasi Route Tertentu

--- Code/praktikum/my-app/src/middleware.ts
+++ Code/praktikum/my-app/src/middleware.ts
@@ -3,6 +3,9 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
-
     // return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/produk", "/about"],
 }

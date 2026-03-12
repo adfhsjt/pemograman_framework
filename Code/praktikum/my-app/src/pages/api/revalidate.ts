8df9a80026bec01a939ca05e0a011da88aebcca8
adfhsjt <ahmadriski207@gmail.com>
2026-03-12 12:38:21 +0700

    Bagian 2 – Tambahkan Parameter Data

--- Code/praktikum/my-app/src/pages/api/revalidate.ts
+++ Code/praktikum/my-app/src/pages/api/revalidate.ts
@@ -1,19 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    revalidate: boolean;
-}
+  revalidate: boolean;
+  message?: string;
+};
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
 ) {
+  if (req.query.data === "produk") {
     try {
-        await res.revalidate("produk/static");
-        return res.status(200).json({ revalidate: true });
-    }catch (error){
-        console.error("Error in API route:", error);
-        return res.status(500).send({ revalidate: false });
+      await res.revalidate("/produk/static");
+      return res.status(200).json({ revalidate: true });
+    } catch (error) {
+      console.error("Error in API route:", error);
+      return res.status(500).send({ revalidate: false });
     }
+  }
+
+  return res.json({
+    revalidate: false,
+    message: "Invalid query parameter. Expected 'data=produk'.",
+  });
 }

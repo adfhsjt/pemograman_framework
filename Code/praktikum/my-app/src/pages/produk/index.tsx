cc0bd95fc069cb623175a34c3f8c69841f8d3778
adfhsjt <ahmadriski207@gmail.com>
2026-03-12 13:32:37 +0700

    Jobsheet 13: Bagian 1 – Membuat Middleware

--- Code/praktikum/my-app/src/pages/produk/index.tsx
+++ Code/praktikum/my-app/src/pages/produk/index.tsx
@@ -6,19 +6,19 @@ import fetcher from "../../utils/swr/fetcher";
 // const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const kategori = () => {
     // const [isLogin, setsLogin] = useState(false);
-    // const {push} = useRouter();
+    const {push} = useRouter();
     const [products, setProducts] = useState([]);
     // Menggunakan SWR 
-    const {data, error, isLoading} = useSWR("http://localhost:3000/api/produk", fetcher);
+    const {data, error, isLoading} = useSWR("/api/produk", fetcher);
     
     // cek apakah data, error, atau isLoading sudah benar
-    console.log("Data:", data);
+    // console.log("Data:", data);
     // console.log("Error:", error);
     // console.log("Is Loading:", isLoading);
 
     return (
         <div className="container mx-auto p-4">
-            <TampilanProduk products={data?.data || []} isLoading={isLoading} />
+            <TampilanProduk products={isLoading ? [] : data.data} />
         </div>
     );
 };

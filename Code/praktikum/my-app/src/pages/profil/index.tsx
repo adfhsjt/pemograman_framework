f057a1611bae238e05cda742f50e8320c807abb1
adfhsjt <ahmadriski207@gmail.com>
2026-03-13 08:37:00 +0700

    E. Proteksi Halaman Profile: Buat Halaman Profile

--- Code/praktikum/my-app/src/pages/profil/index.tsx
+++ Code/praktikum/my-app/src/pages/profil/index.tsx
@@ -1,15 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import {useSession} from "next-auth/react";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const HalamanProfile = () => {
+  const {data}:any = useSession();
   return (
     <div>
-      <h1>Ini Adalah Halaman Profil</h1>
-      <a href="profil/edit">Edit Profil</a>
+      <h1 className="text-2xl font-bold pl-4">Halaman Profile</h1><br />
+      <h1 className="text-2xl font-bold pl-4">Selamat Datang {data?.user?.fullname}</h1><br />
     </div>
   )
 }
+
+export default HalamanProfile;

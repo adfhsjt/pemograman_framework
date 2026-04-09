a939b230ae5974ccd24387e63bf7691549b2970f
adfhsjt <ahmadriski207@gmail.com>
2026-04-09 15:58:34 +0700

    BAGIAN 3 – Authorize di NextAuth (Database Login)

--- Code/praktikum/my-app/src/pages/api/auth/[...nextauth].ts
+++ Code/praktikum/my-app/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { signIn } from "@/utils/db/servicefirebase";
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -10,22 +12,30 @@ export const authOptions: NextAuthOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        fullname: { label: "Full Name", type: "text" },
+        // fullname: { label: "Full Name", type: "text" },
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const user: any = {
-          id: "1",
-          email: credentials?.email,
-          password: credentials?.password,
-          fullname: credentials?.fullname,
-        };
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user: any = await signIn(credentials.email);
         if (user) {
-          return user;
-        } else {
-          return null;
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+          if (isPasswordValid) {
+            // Pastikan mengembalikan objek user yang bersih
+            return {
+              id: user.id,
+              email: user.email,
+              fullname: user.fullname,
+              role: user.role,
+            };
+          }
         }
+        return null;
       },
     }),
   ],
@@ -50,7 +60,7 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  
+
   pages: {
     signIn: "/auth/login",
   },

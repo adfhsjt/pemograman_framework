6e62272795a6178c7c45bef368177bfb28470a25
adfhsjt <ahmadriski207@gmail.com>
2026-04-10 19:16:00 +0700

    Jobsheet 18: PRAKTIKUM 1 – IMAGE OPTIMIZATION: A. Optimasi Gambar Lokal (Public Folder)

--- Code/praktikum/my-app/src/pages/404.tsx
+++ Code/praktikum/my-app/src/pages/404.tsx
@@ -1,12 +1,20 @@
 import styles from "@/styles/404.module.scss";
 import Link from "next/dist/client/link";
+import Image from "next/image";
 const Custom404 = () => {
     return (
         <div className={styles.error}>
             <head>
                 <title>404 - Halaman Tidak Ditemukan</title>
             </head>
-            <img src="/page-not-found.svg" alt="404" className={styles.error__image} />
+            {/* <img src="/page-not-found.svg" alt="404" className={styles.error__image} /> */}
+            <Image
+                src="/page-not-found.svg"
+                alt="404"
+                width={400}
+                height={200}
+                className={styles.error__image}
+            />
             <h2 className={styles.error__subtitle}>
                 Halaman Tidak Ditemukan
             </h2>

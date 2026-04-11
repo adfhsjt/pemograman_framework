529926cbe9301b16d333d4951e6dc71a637020ef
adfhsjt <ahmadriski207@gmail.com>
2026-04-11 08:57:19 +0700

    4. Terapkan dynamic import pada minimal 1 komponen

--- Code/praktikum/my-app/src/pages/_app.tsx
+++ Code/praktikum/my-app/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from 'next/app'
-import Navbar from '@/components/layouts/navbar'
 import AppShell from '@/components/layouts/Appshell';
 import {SessionProvider} from "next-auth/react";
+import dynamic from "next/dynamic";
+
+const Analytics = dynamic(() => import('@/components/analytics/Analytics'), { ssr: false });
 
 export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+    <Analytics />
     <AppShell>
       <Component {...pageProps} />
     </AppShell>
